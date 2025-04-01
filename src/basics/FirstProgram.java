490ab3fbd5812af337d15978d311f9cdee928908
Surjith95 <130816702+Surjith95@users.noreply.github.com>
2025-04-01 14:47:28 +0530

    Update FirstProgram.java

--- src/basics/FirstProgram.java
+++ src/basics/FirstProgram.java
@@ -4,6 +4,7 @@ public class FirstProgram {
 	
 	public static void main(String[] args) {
 		System.out.println("Java is a Programming language");
+		System.out.println("Python is a Programming language");
 	}
 
 }
