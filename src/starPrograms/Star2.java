386813e0633ffa4f646374f7cf71ede4ef92c001
Surjith <manoharsurjith@gmail.com>
2025-05-02 16:28:45 +0530

    star program2

---
+++ src/starPrograms/Star2.java
@@ -0,0 +1,15 @@
+package starPrograms;
+
+public class Star2 {
+	
+	public static void main(String[] args) {
+		int n = 5; 
+		for(int i=1; i<=n; i++) {
+			for(int j=i; j<=5; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
